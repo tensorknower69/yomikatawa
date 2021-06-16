14bb00c33cac021833b69709d81a5ddf0617d9a8
tensorknower69 <latesight20@protonmail.com>
2021-06-16 20:30:02 +0800

    replace tabs to 2 spaces

--- app/Yomikatawa/Main.hs
+++ app/Yomikatawa/Main.hs
@@ -20,101 +20,102 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 
 data ProgramOptions -- ^for scalability
-	= Search'ProgramOptions
-		{ category'Search'ProgramOptions :: Category
-		, inputWord'Search'ProgramOptions :: T.Text
-		, printRomaji'Search'ProgramOptions :: Bool
-		, printRandomWords'Search'ProgramOptions :: Bool
-		, printSameReadingWords'Search'ProgramOptions :: Bool
-		}
-	| Version'ProgramOptions
-	deriving (Show, Eq)
+  = Search'ProgramOptions
+    { category'Search'ProgramOptions :: Category
+    , inputWord'Search'ProgramOptions :: T.Text
+    , printRomaji'Search'ProgramOptions :: Bool
+    , printRandomWords'Search'ProgramOptions :: Bool
+    , printSameReadingWords'Search'ProgramOptions :: Bool
+    }
+  | Version'ProgramOptions
+  deriving (Show, Eq)
 
 programOptionsParser :: Parser ProgramOptions
 programOptionsParser = version <|> search
-	where
-	version = flag' Version'ProgramOptions
-		( short 'v'
-		<> long "version"
-		)
-	search = Search'ProgramOptions <$> search_category <*> search_input_word <*> print_romaji <*> print_random_words <*> print_same_reading_words
-	search_category :: Parser Category
-	search_category = sei <|> mei <|> kanji <|> pure Kanji
-		where
-		sei = flag' Sei
-			( short 's'
-			<> long "sei"
-			<> help "Set category to 性"
-			)
-		mei = flag' Mei
-			( short 'm'
-			<> long "mei"
-			<> help "Set category to 名"
-			)
-		kanji = flag' Kanji
-			( short 'k'
-			<> long "kanji"
-			<> help "Set category to 漢字, this is the default category"
-			)
-	search_input_word = T.pack <$> argument str
-		( metavar "INPUT_WORD"
-		)
-	print_romaji = switch
-		( short 'r'
-		<> long "print-romaji"
-		<> help "Print romaji"
-		)
-	print_random_words = switch
-		( short 'R'
-		<> long "print-random-words"
-		<> help "Print random words"
-		)
-	print_same_reading_words = switch
-		( short 'e'
-		<> long "print-same-reading-words"
-		<> help "Print words with same reading"
-		)
+  where
+  version = flag' Version'ProgramOptions
+    ( short 'v'
+    <> long "version"
+    )
+  search = Search'ProgramOptions <$> search_category <*> search_input_word <*> print_romaji <*> print_random_words <*> print_same_reading_words
+  search_category :: Parser Category
+  search_category = sei <|> mei <|> kanji <|> pure Kanji
+    where
+    sei = flag' Sei
+      ( short 's'
+      <> long "sei"
+      <> help "Set category to 性"
+      )
+    mei = flag' Mei
+      ( short 'm'
+      <> long "mei"
+      <> help "Set category to 名"
+      )
+    kanji = flag' Kanji
+      ( short 'k'
+      <> long "kanji"
+      <> help "Set category to 漢字, this is the default category"
+      )
+  search_input_word = T.pack <$> argument str
+    ( metavar "INPUT_WORD"
+    )
+  print_romaji = switch
+    ( short 'r'
+    <> long "print-romaji"
+    <> help "Print romaji"
+    )
+  print_random_words = switch
+    ( short 'R'
+    <> long "print-random-words"
+    <> help "Print random words"
+    )
+  print_same_reading_words = switch
+    ( short 'e'
+    <> long "print-same-reading-words"
+    <> help "Print words with same reading"
+    )
 
 genericUserAgent :: BS.ByteString
 genericUserAgent = "python-requests/2.25.0"
 
 data ScalpelException
-	= ScalpelException BL.ByteString
-	deriving (Show, Eq)
+  = ScalpelException BL.ByteString
+  deriving (Show, Eq)
 instance Exception ScalpelException
 
 main :: IO ()
 main = do
-	execParser $ info (programOptionsParser <**> helper) (fullDesc <> progDesc "A haskell CLI for https://yomikatawa.com")
-	>>= \case
-		Version'ProgramOptions -> putStrLn "0.1.0.2"
-		Search'ProgramOptions{..} -> do
-			uri <- mkSearchURI category'Search'ProgramOptions inputWord'Search'ProgramOptions
-			mgr <- newTlsManager
-			req' <- parseRequest (renderStr uri)
-			let req = req'
-				{ requestHeaders =
-					[ (hUserAgent, genericUserAgent)
-					, (hHost, "yomikatawa.com")
-					, (hAcceptEncoding, "gzip, deflate")
-					, (hAccept, "*/*")
-					]
-				}
-			rep <- httpLbs req mgr
+  execParser $ info (programOptionsParser <**> helper) (fullDesc <> progDesc "A haskell CLI for https://yomikatawa.com")
+  >>= \case
+    Version'ProgramOptions -> putStrLn "0.1.0.2"
+    Search'ProgramOptions{..} -> do
+      uri <- mkSearchURI category'Search'ProgramOptions inputWord'Search'ProgramOptions
+      mgr <- newTlsManager
+      req' <- parseRequest (renderStr uri)
+      let
+        req = req'
+          { requestHeaders =
+            [ (hUserAgent, genericUserAgent)
+            , (hHost, "yomikatawa.com")
+            , (hAcceptEncoding, "gzip, deflate")
+            , (hAccept, "*/*")
+            ]
+          }
+      rep <- httpLbs req mgr
 
-			case scrapeStringLike (responseBody rep) yomikatawaScraper of
-				Nothing -> throwM $ ScalpelException (responseBody rep)
-				Just (Left ErrorOccurred) -> do
-					putStrLn $ "Error occurred due to input: " <> T.unpack inputWord'Search'ProgramOptions <> "\nMost likely due to invalid input like '+'."
-				Just (Left err) -> throwM err
-				Just (Right r) -> do
-					when (resultMayNotBeCorrect r) $ do
-						putStrLn $ "[!] The translation may not be correct, please check with other dictionary sites."
-					T.putStrLn $ "Hiragana: " <> resultHiragana r
-					when printRomaji'Search'ProgramOptions $ case resultRomaji r of
-						Nothing -> pure ()
-						Just y -> T.putStrLn $ "Romaji: " <> y
-					when (printSameReadingWords'Search'ProgramOptions && (not . null $ resultSameReadingWords r)) $ do
-						T.putStrLn $ "Same reading: " <> (T.intercalate ", " $ resultSameReadingWords r)
-					when (printRandomWords'Search'ProgramOptions && (not . null $ resultRandomWords r)) $ do
-						T.putStrLn $ "Random words: " <> (T.intercalate ", " $ resultRandomWords r)
+      case scrapeStringLike (responseBody rep) yomikatawaScraper of
+        Nothing -> throwM $ ScalpelException (responseBody rep)
+        Just (Left ErrorOccurred) -> do
+          putStrLn $ "Error occurred due to input: " <> T.unpack inputWord'Search'ProgramOptions <> "\nMost likely due to invalid input like '+'."
+        Just (Left err) -> throwM err
+        Just (Right r) -> do
+          when (resultMayNotBeCorrect r) $ do
+            putStrLn $ "[!] The translation may not be correct, please check with other dictionary sites."
+          T.putStrLn $ "Hiragana: " <> resultHiragana r
+          when printRomaji'Search'ProgramOptions $ case resultRomaji r of
+            Nothing -> pure ()
+            Just y -> T.putStrLn $ "Romaji: " <> y
+          when (printSameReadingWords'Search'ProgramOptions && (not . null $ resultSameReadingWords r)) $ do
+            T.putStrLn $ "Same reading: " <> (T.intercalate ", " $ resultSameReadingWords r)
+          when (printRandomWords'Search'ProgramOptions && (not . null $ resultRandomWords r)) $ do
+            T.putStrLn $ "Random words: " <> (T.intercalate ", " $ resultRandomWords r)
--- src/Text/HTML/Scalpel/Yomikatawa.hs
+++ src/Text/HTML/Scalpel/Yomikatawa.hs
@@ -29,10 +29,10 @@ import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
 
 data Category
-	= Kanji -- ^漢字
-	| Mei -- ^名
-	| Sei -- ^性
-	deriving (Show, Eq)
+  = Kanji -- ^漢字
+  | Mei -- ^名
+  | Sei -- ^性
+  deriving (Show, Eq)
 
 categoryPathPiece :: Category -> RText 'PathPiece
 categoryPathPiece Kanji = [pathPiece|kanji|]
@@ -44,29 +44,29 @@ yomikatawaUri = [uri|https://yomikatawa.com|]
 
 mkSearchURI :: (MonadThrow m) => Category -> T.Text -> m URI
 mkSearchURI c x = do
-	x_pp <- mkPathPiece x
-	pure $ yomikatawaUri & uriPath .~ [categoryPathPiece c, x_pp]
+  x_pp <- mkPathPiece x
+  pure $ yomikatawaUri & uriPath .~ [categoryPathPiece c, x_pp]
 
 data Result
-	= Result
-		{ resultMayNotBeCorrect :: Bool
-		-- ^True when @\/\/div[\@class="alert"]\/text() == "このワードは、正しくない可能性があります。他の辞書サイトもご確認ください。"@,
-		--
-		-- translation: This word's translation may not be correct, so please check out other dictionary sites just to make sure.
-		, resultInputWord :: T.Text -- ^The input word.
-		, resultHiragana :: T.Text -- ^The reading of the input word.
-		, resultRomaji :: Maybe T.Text -- ^Romaji of the hiragana. I may implement a translator soon.
-		, resultSameReadingWords :: [T.Text] -- ^A list of words with the same reading.
-		, resultRandomWords :: [T.Text] -- ^A list of random words suggested by the site.
-		}
-	deriving (Show, Eq)
+  = Result
+    { resultMayNotBeCorrect :: Bool
+    -- ^True when @\/\/div[\@class="alert"]\/text() == "このワードは、正しくない可能性があります。他の辞書サイトもご確認ください。"@,
+    --
+    -- translation: This word's translation may not be correct, so please check out other dictionary sites just to make sure.
+    , resultInputWord :: T.Text -- ^The input word.
+    , resultHiragana :: T.Text -- ^The reading of the input word.
+    , resultRomaji :: Maybe T.Text -- ^Romaji of the hiragana. I may implement a translator soon.
+    , resultSameReadingWords :: [T.Text] -- ^A list of words with the same reading.
+    , resultRandomWords :: [T.Text] -- ^A list of random words suggested by the site.
+    }
+  deriving (Show, Eq)
 
 data YomikatawaException
-	= NotFoundError T.Text -- ^When @検索結果: 見つかりませんでした。@.
-	| PleaseInputKanji T.Text -- ^When @漢字を含めてください。@, basically the input is not a kanji.
-	| PleaseDon'tInputAlphaDigits T.Text -- ^When @アルファベット・数字を含めることはできません。@, basically the input shouldn't be alphadigits
-	| ErrorOccurred -- ^Basically 'PleaseInputKanji' but worse...
-	deriving (Show, Eq)
+  = NotFoundError T.Text -- ^When @検索結果: 見つかりませんでした。@.
+  | PleaseInputKanji T.Text -- ^When @漢字を含めてください。@, basically the input is not a kanji.
+  | PleaseDon'tInputAlphaDigits T.Text -- ^When @アルファベット・数字を含めることはできません。@, basically the input shouldn't be alphadigits
+  | ErrorOccurred -- ^Basically 'PleaseInputKanji' but worse...
+  deriving (Show, Eq)
 
 instance Exception YomikatawaException
 
@@ -75,30 +75,30 @@ linksScraper = fmap (T.decodeUtf8 . castString) <$> texts "a"
 
 yomikatawaScraper :: StringLike str => Scraper str (Either YomikatawaException Result)
 yomikatawaScraper = error_scraper <|> normal_scraper
-	where
-	error_scraper = do
-		chroot ("article" @: ["id" @= "content"]) $ do
-			inSerial $ do
-				msg <- T.decodeUtf8 . castString <$> seekNext (text "h1")
-				if msg == "エラーが発生しました。" then pure (Left ErrorOccurred) else empty
-	normal_scraper = do
-		chroot ("section" @: ["id" @= "content"]) $ do
-			runExceptT $ do
-				(alert, input_word, hiragana, may_romaji) <- ExceptT . inSerial . runExceptT $ do
-					alert <- lift $ (seekNext (text ("div" @: [hasClass "alert"])) $> True) <|> pure False
-					input_word <- input_word_scraper
-					hiragana <- ExceptT $ parse_hiragana input_word . T.decodeUtf8 . castString <$> seekNext (text "p")
-					may_romaji <- lift $ (Just . castString <$> seekNext (text "p")) <|> pure Nothing
-					pure (alert, input_word, hiragana, may_romaji)
-				same_reading_words <- lift $ chroot ("section" @: ["id" @= "sameReadingWords", hasClass "links"]) linksScraper <|> pure mempty
-				random_words <- lift $ chroot ("section" @: ["id" @= "randomWords", hasClass "links"]) linksScraper
-				pure $ Result alert input_word hiragana may_romaji same_reading_words random_words
-	input_word_scraper = lift $ T.decodeUtf8 . castString <$> seekNext (text ("h1" // "strong") <|> text ("h2" // "strong"))
-	parse_hiragana input_word hiragana
-		| "検索結果: 見つかりませんでした。" == hiragana
-			= Left $ NotFoundError input_word
-		| T.isPrefixOf "漢字を含めてください。" hiragana
-			= Left $ PleaseInputKanji input_word
-		| T.isSuffixOf "アルファベット・数字を含めることはできません。" hiragana
-			= Left $ PleaseDon'tInputAlphaDigits input_word
-		| True = pure $ hiragana
+  where
+  error_scraper = do
+    chroot ("article" @: ["id" @= "content"]) $ do
+      inSerial $ do
+        msg <- T.decodeUtf8 . castString <$> seekNext (text "h1")
+        if msg == "エラーが発生しました。" then pure (Left ErrorOccurred) else empty
+  normal_scraper = do
+    chroot ("section" @: ["id" @= "content"]) $ do
+      runExceptT $ do
+        (alert, input_word, hiragana, may_romaji) <- ExceptT . inSerial . runExceptT $ do
+          alert <- lift $ (seekNext (text ("div" @: [hasClass "alert"])) $> True) <|> pure False
+          input_word <- input_word_scraper
+          hiragana <- ExceptT $ parse_hiragana input_word . T.decodeUtf8 . castString <$> seekNext (text "p")
+          may_romaji <- lift $ (Just . castString <$> seekNext (text "p")) <|> pure Nothing
+          pure (alert, input_word, hiragana, may_romaji)
+        same_reading_words <- lift $ chroot ("section" @: ["id" @= "sameReadingWords", hasClass "links"]) linksScraper <|> pure mempty
+        random_words <- lift $ chroot ("section" @: ["id" @= "randomWords", hasClass "links"]) linksScraper
+        pure $ Result alert input_word hiragana may_romaji same_reading_words random_words
+  input_word_scraper = lift $ T.decodeUtf8 . castString <$> seekNext (text ("h1" // "strong") <|> text ("h2" // "strong"))
+  parse_hiragana input_word hiragana
+    | "検索結果: 見つかりませんでした。" == hiragana
+      = Left $ NotFoundError input_word
+    | T.isPrefixOf "漢字を含めてください。" hiragana
+      = Left $ PleaseInputKanji input_word
+    | T.isSuffixOf "アルファベット・数字を含めることはできません。" hiragana
+      = Left $ PleaseDon'tInputAlphaDigits input_word
+    | True = pure $ hiragana
